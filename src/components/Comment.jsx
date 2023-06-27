eb133c5a96bf69ab572cbddf375c03629e0f5ebb
Gabriel Paulo <gabrielrodriguespaulo15@gmail.com>
2023-06-27 17:43:28 -0300

    Finalizando o Comment.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment() {
     return (
-        <div className={styles.comment}></div>
+        <div className={styles.comment}>
+            <img src="https://github.com/GabrielRPaulo.png" alt="Foto de perfil" />
+
+            <div className={styles.commentBox}>
+
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Paulo</strong>
+                            <time title="05 de junho de 2023 às 08:13H" dateTime="2023-06-05 08:13:30">Cerca de 1h atrás</time>
+
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
     )
 }
