1c1a53983370e2c2123da1e592111cb83a1b6581
Gabriel Paulo <gabrielrodriguespaulo15@gmail.com>
2023-06-29 16:29:54 -0300

    Criando o componente "Avatar"

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/GabrielRPaulo.png" alt="Foto de perfil" />
+            <Avatar hasBorder={false} src="https://github.com/GabrielRPaulo.png"  />
 
             <div className={styles.commentBox}>
 
